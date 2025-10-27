414f583c3c6e5a28a4109239bdc3dc053b46667f
rahmanmushfiqur39 <rahman.mushfiqur39@gmail.com>
2025-10-27 13:42:11 +0000

    Update ecr_repd_search_tool.py

--- ecr_repd_search_tool.py
+++ ecr_repd_search_tool.py
@@ -122,7 +122,7 @@ def compute_match(base_row, search_row, text_thresh, base_cols, search_cols,
         token_set_a = fuzz.token_set_ratio(base_text_a, search_text_a)
         token_sort_a = fuzz.token_sort_ratio(base_text_a, search_text_a)
         partial_a = fuzz.partial_ratio(base_text_a, search_text_a)
-        text_score_a = max(token_set_a, 0)
+        text_score_a = max(token_set_a, token_sort_a, partial_a)
         if text_score_a >= text_thresh:
             reasons.add("Text (GrpA)")
             base_details.append(f"tA: {base_text_a}")
@@ -136,7 +136,7 @@ def compute_match(base_row, search_row, text_thresh, base_cols, search_cols,
         token_set_b = fuzz.token_set_ratio(base_text_b, search_text_b)
         token_sort_b = fuzz.token_sort_ratio(base_text_b, search_text_b)
         partial_b = fuzz.partial_ratio(base_text_b, search_text_b)
-        text_score_b = max(token_set_b, 0)
+        text_score_b = max(token_set_b, token_sort_b, partial_b)
         if text_score_b >= text_thresh:
             reasons.add("Text (GrpB)")
             base_details.append(f"tB: {base_text_b}")
